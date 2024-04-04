2fdf00143ca91a8e863804d1af8f33550a67c946
Cameron Sunderman <CamSund02@outlook.com>
2024-04-03 20:29:28 -0500

    part 8 update

--- controllers/gizmo.js
+++ controllers/gizmo.js
@@ -28,9 +28,26 @@ exports.gizmo_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Gizmo detail: ' + req.params.id);
 };
 // Handle Gizmo create on POST.
-exports.gizmo_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Gizmo create POST');
-};
+// Handle Gizmo create on POST.
+exports.gizmo_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Gizmo();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"gizmo_type":"goat", "cost":12, "size":"large"}
+    document.gizmo_type = req.body.gizmo_name;
+    document.cost = req.body.price;
+    document.size = req.body.functionality;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
 // Handle Gizmo delete from on DELETE.
 exports.gizmo_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Gizmo delete DELETE ' + req.params.id);
