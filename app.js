17f41c5b41d176352a480e4c782a0d9eb26d1779
Cameron Sunderman <CamSund02@outlook.com>
2024-04-08 03:00:36 -0500

    Lab 13 part 2

--- app.js
+++ app.js
@@ -39,6 +39,26 @@ var logger = require('morgan');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
+//NOT SURE IF THIS IS SUPPOSED TO GO HERE
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  )
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var gizmoRouter = require('./routes/gizmo');
@@ -56,6 +76,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//Assignment 12 part 2 sub 4
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+//Assignmetn 12 part 2 sub 4 END
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -64,6 +95,16 @@ app.use('/gizmo', gizmoRouter);
 app.use('/grid', gridRouter);
 app.use('/randomitem', pickRouter)
 app.use('/resource', resourceRouter)
+
+//Assignment 12 part 2 sub 5
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+//Assignment 12 part 2 sub 5 END
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
